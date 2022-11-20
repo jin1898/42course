2f29d37aefe706f241dfaec8a1e4b2c96a7bbb6c
asteadygoer <asteadygoer@naver.com>
2022-11-20 15:07:58 +0900

    22/11/20//15:07 libft_itoa

---
+++ libft/libft.h
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char	*ft_itoa(int n);
+char	**ft_split(const char *s, char c);
---
+++ libft/main.c
@@ -0,0 +1,7 @@
+#include "libft.h"
+
+int	main(void)
+{
+	printf("num = %d\n",-123);
+	printf("itoa_answer : %s\n",ft_itoa(-123));
+}
