453fc4bcafebd5c81931b44a98ac67cc4aeb7de8
jin1898 <jinasunwoo@naver.com>
2023-11-06 15:31:55 +0900

    ex06하는중

---
+++ cpp00-cpp04/cpp01/ex06/main.cpp
@@ -0,0 +1,15 @@
+#include "Harl.hpp"
+
+
+int main(int ac, char **av)
+{
+	Harl H;
+
+	(void)ac;
+	(void)av;
+	// H.complain("DEBUG");
+	// H.complain("INFO");
+	// H.complain("WARNING");
+	// H.complain("ERROR");
+	H.complain("asdfasdf");
+}
